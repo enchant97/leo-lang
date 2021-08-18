2ba655ba91dcc7ec4090a8d1dbaab0dfcc6a97e1
Leo Spratt <enchant97@users.noreply.github.com>
2021-08-18 20:29:21 +0100

    add inline strings with OUT

--- src/writers.c
+++ src/writers.c
@@ -44,14 +44,31 @@ void write_variable(FILE *fp, Char_Slice curr_line) {
 }
 
 void write_out(FILE *fp, Char_Slice curr_line) {
-  char *variable_name = curr_line.array[2];
   Std_Streams stream_type = char_to_std_stream(curr_line.array[1]);
+  char *print_content;
+  if (strncmp(curr_line.array[2], "\"", 1) == 0) {
+    // print from a inline string
+    int end_i = curr_line.rows - 1;
+    if (strcmp(&curr_line.array[end_i][strlen(curr_line.array[end_i]) - 1],
+               "\"") != 0) {
+      fprintf(stderr, "missing double quotes around defined string");
+      exit(EXIT_FAILURE);
+    }
+    print_content = combine_sliced(curr_line, 2, end_i, " ");
+  } else {
+    // print from a variable
+    if (curr_line.rows > 3) {
+      fprintf(stderr, "invalid OUT command");
+      exit(EXIT_FAILURE);
+    }
+    print_content = curr_line.array[2];
+  }
   switch (stream_type) {
   case STDOUT:
-    fprintf(fp, "printf(\"%%s\", %s);", variable_name);
+    fprintf(fp, "printf(\"%%s\", %s);", print_content);
     break;
   case STDERR:
-    fprintf(fp, "fprintf(stderr, \"%%s\", %s);", variable_name);
+    fprintf(fp, "fprintf(stderr, \"%%s\", %s);", print_content);
     break;
   default:
     fprintf(stderr, "unhandled std stream type %s", curr_line.array[1]);
